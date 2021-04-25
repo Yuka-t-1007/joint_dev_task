b1c06abdf58a3fce131a90c0d99b93b34753f798
yuka <tyty10072019@gmail.com>
2021-04-25 16:31:10 +0900

    Q6~Q11まで回答

--- task.rb
+++ task.rb
@@ -53,14 +53,15 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map{|number|number*10}
+  puts numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -69,7 +70,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map(&:capitalize!)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -80,21 +82,37 @@ def q9
 
   # 以下に回答を記載
 
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i}  #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+  foods.each do |i|
+    if i.include?("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
+ end
 
-end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
-
+  
   # 以下に回答を記載
-
+  sports.flatten!
+  sports.uniq!
+  sports.each.with_index(1) do |name,i|
+    puts "No#{i} #{name}"
+  end
 end
+
+#########4/25 ここまで終了############
 
 def q12
   data = { user: { name: "satou", age: 33 } }
@@ -108,6 +126,8 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
+  user_dara = update_data
+  puts user_data
 
 end
 
